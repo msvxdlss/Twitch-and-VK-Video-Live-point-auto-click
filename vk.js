819d01910800530b587d066f471d3c6eb429fad8
Димка <ya.lpy@ya.ru>
2024-08-21 23:01:30 +0300

    Update vk.js

--- vk.js
+++ vk.js
@@ -1,8 +1,6 @@
-function checkPoint() {
+setInterval(() => {
     const icon = document.querySelector('.PointActions_buttonBonus_F37Ur');
-    if (!icon) return;
-    icon.click();
-}
-setTimeout(function() {
-    setInterval(checkPoint, 5000);
-  }, 5000);
+    if (icon) {
+      icon.click();
+    }
+  }, 30000);
